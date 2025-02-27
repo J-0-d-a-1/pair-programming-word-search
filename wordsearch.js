18f6e206c2a8c56d1a0548874fc967a6b6a3a7f0
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-26 21:16:40 -0800

    wordSerch vertical case added

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  let transposedArr = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    transposedArr.push([]);
+  }
+
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      transposedArr[j][i] = letters[i][j];
+    }
+  }
+
+  const verticalJoin = transposedArr.map((ls) => ls.join(""));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
